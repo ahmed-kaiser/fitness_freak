8cacb19b4e71ffabfee2209c154e55a08f615eea
Kaiser <ahmedkaiser.001@gmail.com>
2022-09-28 23:19:35 +0600

    added db.json and image data in public folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [exercise, setExercise] = useState([])
+
+  useEffect(() => {
+    fetch('./db.json')
+      .then(response => response.json())
+      .then(data => setExercise(data))
+  }, []);
+  console.log(exercise);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        exercise.map(item => (
+          <div>
+            <p>{item.name}</p>
+            <img src={item.image} alt="..."/>
+          </div>
+        ))
+      }
     </div>
   );
 }
